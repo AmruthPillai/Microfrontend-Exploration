90f77f24b9b25f5a348c10a06043c404ba39ffd0
Amruth Pillai <im.amruth@gmail.com>
2021-10-09 11:16:03 +0200

    setup github action to listen on main

--- ecommerce-vanilla/container/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import { mount as mountProducts } from "products/ProductsList";
-import { mount as mountShoppingCart } from "cart/ShoppingCart";
-
-mountProducts(document.querySelector("#productList"));
-mountShoppingCart(document.querySelector("#shoppingCart"));
