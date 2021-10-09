b68255698a377c2e0f3186f405ace94c91c9ac7d
Amruth Pillai <im.amruth@gmail.com>
2021-10-09 10:57:30 +0200

    prepare for ci/cd with github actions

---
+++ module-federation-project/container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJSON = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      shared: packageJSON.dependencies,
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
--- module-federation-project/marketing/config/webpack.dev.js
+++ module-federation-project/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJSON = require("../package.json");
@@ -20,9 +19,6 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
